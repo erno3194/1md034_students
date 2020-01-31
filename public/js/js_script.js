a10bccd08bc32bab5ffa8a2ecc257422931d052b
erno3194 <erik.noren.3194@student.uu.se>
2020-01-31 15:40:17 +0100

    added menu items in js file

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,1 +1,29 @@
-l
+function menuItem(name, info, allergyLac, allergyGlu, kcal, img){
+    this.name = name;
+    this.info = info;
+    this.allergyLac = allergyLac;
+    this.allergyGlu = allergyGlu;
+    this.kcal = kcal;
+    this.img = img;
+
+
+    this.getKcal = function() {
+	return this.name + ' ' + this.kcal;
+    };
+}
+
+let item1 = new menuItem('Big eMac', ' ', 'yes', 'yes', 507, 'img/bigmac_460x330.jpg');
+
+let item2 = new menuItem('Bigger eMac', ' ', 'yes', 'yes', 1014, 'img/bigmac_460x330.jpg');
+
+let item3 = new menuItem('Biggest eMac', ' ', 'yes', 'yes', 2028, 'img/bigmac_460x330.jpg');
+
+let item4 = new menuItem('Biggerest eMac', ' ', 'yes', 'yes', 4056, 'img/bigmac_460x330.jpg');
+
+let item5 = new menuItem('Biggestest eMac', ' ', 'yes', 'yes', 8112, 'img/bigmac_460x330.jpg');
+
+console.log( item1.getKcal() );
+console.log( item2.getKcal() );
+console.log( item3.getKcal() );
+console.log( item4.getKcal() );
+console.log( item5.getKcal() );
