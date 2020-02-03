c501b73e235aeef2d5b68cdd3ffd1d18f57b7188
erno3194 <erik.noren.3194@student.uu.se>
2020-02-03 12:45:49 +0100

    js_script.js is now synced with step 4-6 jsfiddle

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -6,24 +6,19 @@ function menuItem(name, info, allergyLac, allergyGlu, kcal, img){
     this.kcal = kcal;
     this.img = img;
 
-
     this.getKcal = function() {
-	return this.name + ' ' + this.kcal;
+    	return this.name + ' ' + this.kcal + ' kcal';
     };
 }
 
 let item1 = new menuItem('Big eMac', ' ', 'yes', 'yes', 507, 'img/bigmac_460x330.jpg');
-
 let item2 = new menuItem('Bigger eMac', ' ', 'yes', 'yes', 1014, 'img/bigmac_460x330.jpg');
-
 let item3 = new menuItem('Biggest eMac', ' ', 'yes', 'yes', 2028, 'img/bigmac_460x330.jpg');
-
 let item4 = new menuItem('Biggerest eMac', ' ', 'yes', 'yes', 4056, 'img/bigmac_460x330.jpg');
-
 let item5 = new menuItem('Biggestest eMac', ' ', 'yes', 'yes', 8112, 'img/bigmac_460x330.jpg');
 
-console.log( item1.getKcal() );
-console.log( item2.getKcal() );
-console.log( item3.getKcal() );
-console.log( item4.getKcal() );
-console.log( item5.getKcal() );
+document.getElementById("b1").innerHTML = item1.getKcal();
+document.getElementById("b2").innerHTML = item2.getKcal();
+document.getElementById("b3").innerHTML = item3.getKcal();
+document.getElementById("b4").innerHTML = item4.getKcal();
+document.getElementById("b5").innerHTML = item5.getKcal();
