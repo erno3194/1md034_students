9586775f8498398317b2337eaf101ef89e0a103c
erno3194 <erik.noren.3194@student.uu.se>
2020-02-13 13:55:49 +0100

    task 5-3

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -28,46 +28,54 @@ const info = new Vue({
 	house: "",
 	payment_method: "",
 	gender: "",
+
+	order: {
+	    orderId: "",
+	    details: {x: 0, y: 0},
+	    orderItems: [],
+	},
+
+	i: 0,
 	
 	orders: {}
     },
-
-
-    created: function() {
-	socket.on('initialize', function(data) {
-	    this.orders = data.orders;
-	}.bind(this));
-
-	socket.on('currentQueue', function(data) {
-	    this.orders = data.orders;
-	}.bind(this));
-    },
-    
-    
+  
     methods: {
 	
 	getNext: function() {
-	    let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
+	   /* let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
 		return Math.max(last, next);
 	    }, 0);
-	    return lastOrder + 1;
+	    return lastOrder + 1;*/
+	    return this.i+1;
+	    
 	},
 	
 	addOrder: function(event) {
+	    socket.emit('addOrder', {
+		orderId: this.getNext(),
+		details: {
+		    x: this.order.details.x,
+		    y: this.order.details.y,
+		},
+		orderItems: vm.getOrder(),
+	    });
+	},
 
+	displayOrder: function(event) {
 	    let offset = {
 		x: event.currentTarget.getBoundingClientRect().left,
 		y: event.currentTarget.getBoundingClientRect().top,
 	    };
-	    socket.emit('addOrder', {
-		orderId: this.getNext(),
+	    this.order = {
+		orderId: "T",
 		details: {
 		    x: event.clientX - 10 - offset.x,
 		    y: event.clientY - 10 - offset.y,
 		},
 		orderItems: ['Beans', 'Curry'],
-	    });
-	},
+	    };
+	},	
 
 	markDone: function(fullname, email, payment_method, gender){
 	    console.log(fullname);
@@ -84,7 +92,7 @@ const info = new Vue({
 	    let genderItem = document.createElement("li");
 	    genderItem.innerHTML = gender;
 	    
-	    //this.addOrder();
+	    this.addOrder();
 	    
 	    divElement.appendChild(nameItem);
 	    divElement.appendChild(emailItem);
@@ -99,6 +107,5 @@ const info = new Vue({
 	    }
 	},
     }
-    
 })
 
